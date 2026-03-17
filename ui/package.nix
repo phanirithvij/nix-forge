99ce91d3f917837a1f1baf6b338dd7f1f42bd9d7
phanirithvij <phanirithvij2000@gmail.com>
2026-03-17 18:39:44 +0530

    feat(ui): re-add forge-options to package build
    This reverts and modifies commit
    e98c8d694691b5837d3980a88efb1c8c25ee9c01.

--- ui/package.nix
+++ ui/package.nix
@@ -5,7 +5,7 @@
   symlinkJoin,
 
   _forge-config,
-  ...
+  _forge-options,
 }:
 
 let
@@ -16,6 +16,17 @@ let
     elmLock = ./elm.lock;
     entry = [ "src/Main.elm" ];
     output = "js/Elm.js";
+    doMinification = true;
+    enableOptimizations = true;
+  };
+
+  options = buildElmApplication {
+    pname = "forge-ui-options";
+    version = "0.1.0";
+    src = ./.;
+    elmLock = ./elm.lock;
+    entry = [ "src/OptionsMain.elm" ];
+    output = "options.js";
     doMinification = true;
     enableOptimizations = true;
   };
@@ -31,12 +42,16 @@ let
 in
 symlinkJoin {
   name = "forge-ui";
-  paths = [ main ];
+  paths = [
+    main
+    options
+  ];
   postBuild = ''
     pushd $out
 
     # Copy static files
     cp ${./src/index.html} index.html
+    cp ${./src/options.html} options.html
     cp -aR ${./src/css}/. css
     cp -aR ${./src/js}/. js
     chmod -R u+w css js
@@ -44,15 +59,19 @@ symlinkJoin {
 
     # Symlink config files
     ln -s ${_forge-config} forge-config.json
+    ln -s ${_forge-options} options.json
 
     # Rename minimized Elm output
     mv js/Elm.min.js js/Elm.js
+    mv options.min.js js/options.js
 
     # github pages SPA workaround for routing
     for app in $(${jq}/bin/jq '.apps.[].name' -r forge-config.json); do
       mkdir -p "app/$app"
       ln -s $out/index.html "app/$app/index.html"
     done
+
+    ln -s $out/options.html options/index.html
 
     popd
   '';
