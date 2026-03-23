b3d40370e016298d783be7bfc39a4e6aea5fe7e0
phanirithvij <phanirithvij2000@gmail.com>
2026-03-23 15:23:10 +0530

    WIP add usage instructions
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- forge/modules/apps/default.nix
+++ forge/modules/apps/default.nix
@@ -69,6 +69,9 @@ in
               in
               # Passthru
               appDrv.overrideAttrs (_: {
+                shellHook = ''
+                  echo ${app.usage}
+                '';
                 passthru = appPassthru app appDrv;
               });
 
