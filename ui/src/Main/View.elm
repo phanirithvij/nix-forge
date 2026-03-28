c8db2a79983974feed191636cd3c1629aa17864f
phanirithvij <phanirithvij2000@gmail.com>
2026-03-28 16:51:16 +0530

    fix(ui): move recipe to metadata and add commit id
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- ui/src/Main/View.elm
+++ ui/src/Main/View.elm
@@ -20,6 +20,10 @@ import Main.Update exposing (..)
 import Main.View.Instructions exposing (..)
 
 
+commit =
+    ":master"
+
+
 view : Model -> Html Update
 view model =
     div
@@ -285,7 +289,6 @@ viewPageApp model pageApp =
             ]
         , viewPageAppTabs model pageApp
         , viewPageAppTabContent model pageApp
-        , viewRecipeLink model pageApp
         , viewPageAppRun model pageApp
         ]
 
@@ -362,6 +365,7 @@ viewTabMetadata model pageApp =
                     [ a [ href "#", target "_blank" ] [ text "Documentation" ] ]
                 , li [ class "list-group-item bg-transparent px-0" ]
                     [ a [ href "#", target "_blank" ] [ text "Source Repository" ] ]
+                , viewRecipeLink model pageApp
                 ]
             ]
         , div [ class "col-md-6" ]
@@ -474,13 +478,18 @@ viewPageAppNgiSubgrants model pageApp =
 
 viewRecipeLink : Model -> PageApp -> Html update
 viewRecipeLink model pageApp =
-    div []
-        [ text "Recipe: "
-        , a
+    li [ class "list-group-item bg-transparent px-0" ]
+        [ a
             [ href
                 (String.join "/"
                     [ model.model_config.config_repository |> showNixUrl
-                    , "blob/master"
+                    , "blob/"
+                        ++ (if not (String.contains "master" commit) then
+                                commit
+
+                            else
+                                "master"
+                           )
                     , model.model_config.config_recipe.configRecipe_apps
                     , pageApp.pageApp_app.app_name
                     , "recipe.nix"
@@ -488,7 +497,9 @@ viewRecipeLink model pageApp =
                 )
             , target "_blank"
             ]
-            [ text (model.model_config.config_recipe.configRecipe_apps ++ "/" ++ pageApp.pageApp_app.app_name ++ "/recipe.nix") ]
+            [ text "Recipe Definition" ]
+
+        -- [ text (model.model_config.config_recipe.configRecipe_apps ++ "/" ++ pageApp.pageApp_app.app_name ++ "/recipe.nix") ]
         ]
 
 
@@ -669,11 +680,7 @@ viewPoweredBy =
                 [ text "ngi-nix/forge" ]
             , text "."
             ]
-        , let
-            commit =
-                ":master"
-          in
-          if not (String.contains "master" commit) then
+        , if not (String.contains "master" commit) then
             span []
                 [ text " Version "
                 , a
