217d1f0a5dc783a582b40474e2431e7e7e757847
phanirithvij <phanirithvij2000@gmail.com>
2026-04-01 02:12:18 +0530

    fix(ui): strip out trailing slash in urls on inital page load
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- ui/src/js/main.js
+++ ui/src/js/main.js
@@ -9,11 +9,23 @@ import {
 const startingFlakePreference = getInitialFlakePreference();
 const startingTheme = getInitialTheme();
 
+// work around github pages adding extra trailing slash
+if (
+  window.location.pathname.endsWith("/") &&
+  window.location.pathname !== "/"
+) {
+  const cleanUrl =
+    window.location.pathname.slice(0, -1) +
+    window.location.search +
+    window.location.hash;
+  window.history.replaceState(null, "", cleanUrl);
+}
+
 // init state
 const app = Elm.Main.init({
   node: document.getElementById("elm-main"),
   flags: {
-    href: location.href,
+    href: window.location.href,
     theme: startingTheme,
     prefersFlakes: startingFlakePreference,
   },
