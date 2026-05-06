f40d4a0ca3b8c32e9f57803e95a21ddd673bad2a
phanirithvij <phanirithvij2000@gmail.com>
2026-05-06 20:34:36 +0530

    test(forge): add docker container engine tests

--- forge/modules/apps/test/container.nix
+++ forge/modules/apps/test/container.nix
@@ -25,16 +25,37 @@
         name = "${app.name}-container-test";
         nodes.machine = {
           virtualisation.podman.enable = true;
+          virtualisation.docker.enable = true;
           virtualisation.containers.enable = true;
           virtualisation.diskSize = 4096;
           system.stateVersion = "25.11";
-          environment.systemPackages = app.programs.packages ++ config.packages ++ [ pkgs.podman-compose ];
+          environment.systemPackages =
+            app.programs.packages
+            ++ config.packages
+            ++ [
+              pkgs.podman-compose
+              pkgs.docker-compose
+            ];
         };
         testScript = ''
           machine.start()
           machine.wait_for_unit("multi-user.target")
-          machine.succeed("${lib.getExe containerRuntime.result.build} --detach")
-          machine.succeed("${pkgs.writeShellScript "${app.name}-container-test-script" config.script}")
+          machine.wait_for_unit("docker.service")
+
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-podman} -d")
+          machine.succeed("${pkgs.writeShellScript "${app.name}-podman-test-script" config.script}")
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-podman-compose} down")
+
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-docker} -d")
+          machine.succeed("${pkgs.writeShellScript "${app.name}-docker-test-script" config.script}")
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-docker-compose} down")
+
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-container} -d")
+          machine.succeed("${pkgs.writeShellScript "${app.name}-auto-test-script" config.script}")
+          machine.succeed("${lib.getExe containerRuntime.result.build.run-podman-compose} down")
+          machine.succeed("PATH=$(echo $PATH | sed 's/[^:]*podman[^:]*://g') ${lib.getExe containerRuntime.result.build.run-container} -d")
+
+          machine.succeed("${pkgs.writeShellScript "${app.name}-auto-docker-test-script" config.script}")
         '';
       }).overrideTestDerivation
         (_: lib.optionalAttrs (!config.sandbox) { __noChroot = true; })
