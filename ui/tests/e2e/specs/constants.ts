fe71b8827004a3d40c98ba46baf23821b167472e
phanirithvij <phanirithvij2000@gmail.com>
2026-07-26 22:10:59 +0530

    fix: mock-test pkg name
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- ui/tests/e2e/specs/constants.ts
+++ ui/tests/e2e/specs/constants.ts
@@ -2,8 +2,8 @@ export const BASE_URL = process.env.BASE_URL || "http://127.0.0.1:3000";
 const isProd = BASE_URL.includes("netlify.app") || BASE_URL.includes("github.io");
 
 export const TEST_APP_NAME = process.env.TEST_APP_NAME || (isProd ? "hello-web" : "mock-test");
-export const TEST_PKG_NAME = process.env.TEST_PKG_NAME || (isProd ? "hello-web" : "mock-test-package");
+export const TEST_PKG_NAME = process.env.TEST_PKG_NAME || (isProd ? "hello-web" : "mock-test-pkg");
 export const TEST_APP_SEARCH = process.env.TEST_APP_SEARCH || (isProd ? "hello-web" : "mock-test");
-export const TEST_PKG_SEARCH = process.env.TEST_PKG_SEARCH || (isProd ? "hello-web" : "mock-test");
+export const TEST_PKG_SEARCH = process.env.TEST_PKG_SEARCH || (isProd ? "hello-web" : "mock-test-pkg");
 
 export const TEST_RECIPE_OPTION = process.env.TEST_RECIPE_OPTION || "apps.<name>.description";
